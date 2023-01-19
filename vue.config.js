535c871ec4492c5292fabb396dbb146a783dbd77
rebecca <rebecca.schmidt@htwg-konstanz.de>
2023-01-19 19:15:18 +0100

    add PWA

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pwa: {
+    name: "phase10_web_vue",
+    themeColor: '#efa032',
+    mobileWebAppCapable: 'yes',
+    mobileWebAppCache:'yes',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppCache: 'yes',
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      navigateFallback: '/index.html',
+      skipWaiting: true,
+      clientsClaim: true
+    },
+    manifestOptions: {
+      name: "Malefiz",
+      short_name: "Malefiz",
+      start_url: '.',
+      display: 'standalone',
+      theme_color: '#efa032'
+    }
+  }
 })
